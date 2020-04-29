442e75c14b5495c059b9027cf6348e4d8e67cb37
zhangcy <zcy0521@gmail.com>
2020-04-29 22:12:05 +0800

    init

--- src/main/java/com/sample/springboot/build/jar/Application.java
+++ src/main/java/com/sample/springboot/build/jar/Application.java
@@ -1,4 +1,4 @@
-package com.sample.jar.springboot;
+package com.sample.springboot.build.jar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/sample/springboot/build/jar/controller/IndexController.java
+++ src/main/java/com/sample/springboot/build/jar/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.sample.jar.springboot.controller;
+package com.sample.springboot.build.jar.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
