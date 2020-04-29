2fb0b298aba1f90327f4f7338d8c9646184d9016
zhangcy <zcy0521@gmail.com>
2020-04-29 16:45:04 +0800

    init

---
+++ src/main/java/com/sample/docker/Application.java
@@ -0,0 +1,17 @@
+package com.sample.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring-boot: https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle
+ * spring-boot-docker: https://spring.io/guides/gs/spring-boot-docker
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
